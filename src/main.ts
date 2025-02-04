612ba6b5942f370feaae33e6724044f57f6b6810
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-02-04 03:51:36 +0000

    created endcoder-decoder manager

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
 import "./styles/style.css";
+
+class EncoderDecoderManager {
+  private inputElement: HTMLTextAreaElement;
+  private outputElement: HTMLTextAreaElement;
+  private EncodeButton: HTMLButtonElement;
+  private DecodeButton: HTMLButtonElement;
+  private CopyButton: HTMLButtonElement;
+  private ClearButton: HTMLButtonElement;
+
+  constructor() {
+    // TextArea Elements
+    this.inputElement = document.querySelector(
+      "textarea#input"
+    ) as HTMLTextAreaElement;
+    this.outputElement = document.querySelector(
+      "textarea#output"
+    ) as HTMLTextAreaElement;
+
+    // Button Elements
+    this.EncodeButton = document.querySelector(
+      "textarea#encode-button"
+    ) as HTMLButtonElement;
+    this.DecodeButton = document.querySelector(
+      "textarea#decode-button"
+    ) as HTMLButtonElement;
+    this.CopyButton = document.querySelector(
+      "textarea#copy-button"
+    ) as HTMLButtonElement;
+    this.ClearButton = document.querySelector(
+      "textarea#clear-button"
+    ) as HTMLButtonElement;
+  }
+}
+
+// Initializing encoder-decoder manager
+const encoderDecoderManager = new EncoderDecoderManager();
